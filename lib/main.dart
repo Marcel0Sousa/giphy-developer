743e6b367321328f377f702157120b7e92042be2
Marcelo Sousa <tchelospy@gmail.com>
2020-02-17 07:10:42 -0300

    Frist Commit App Giphy Developer

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,11 @@
+import 'package:buscador_gifs/ui/home_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
+    theme: ThemeData(primaryColor: Colors.white),
+    debugShowCheckedModeBanner: false,
   ));
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
 }
